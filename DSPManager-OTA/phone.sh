3aa255a18b818b24a532d0a8b782f24a45966ee6
Antti S. Lankila <alankila@gmail.com>
2010-08-19 06:38:12 +0300

    Path upd

--- DSPManager-OTA/phone.sh
+++ DSPManager-OTA/phone.sh
@@ -5,9 +5,7 @@ set -e
 cd $(dirname "$0")
 
 adb remount
-# boot failure if used during boot.
 mkdir -p dsp/system/lib
-cp -v ~/cyanogenmod/out/target/product/hero/system/lib/libaudioflinger.so dsp/system/lib/
+cp -v ~/git/cyanogenmod/out/target/product/hero/system/lib/libaudioflinger.so dsp/system/lib/
 adb push dsp/system/lib/libaudioflinger.so /system/lib/
-#adb push ~/Työpöytä/DSPManager.apk /system/app/
 adb shell kill -9 '$(pidof mediaserver)'
--- DSPManager-OTA/update.sh
+++ DSPManager-OTA/update.sh
@@ -11,8 +11,8 @@ rm -f dsp-signed.zip
 echo "Update APK & .SO"
 mkdir -p dsp/system/lib
 mkdir -p dsp/system/app
-cp -v ~/cyanogenmod/out/target/product/hero/system/lib/libaudioflinger.so dsp/system/lib/
-cp -v ~/android_packages_apps_DSPManager.git/bin/DSPManager.apk dsp/system/app/
+cp -v ~/git/cyanogenmod/out/target/product/hero/system/lib/libaudioflinger.so dsp/system/lib/
+cp -v ~/git/android_packages_apps_DSPManager.git/bin/DSPManager.apk dsp/system/app/
 
 echo "Build zip";
 cd dsp/
@@ -20,7 +20,7 @@ zip -r ../dsp.zip .
 cd ..
 
 echo "Sign zip"
-java -jar ~/cyanogenmod/out/host/linux-x86/framework/signapk.jar \
- ~/cyanogenmod/build/target/product/security/testkey.x509.pem \
- ~/cyanogenmod/build/target/product/security/testkey.pk8 \
+java -jar ~/git/cyanogenmod/out/host/linux-x86/framework/signapk.jar \
+ ~/git/cyanogenmod/build/target/product/security/testkey.x509.pem \
+ ~/git/cyanogenmod/build/target/product/security/testkey.pk8 \
  dsp.zip dsp-signed.zip
