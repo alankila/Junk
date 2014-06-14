719555070e2d12d180425e815ebac4a5b3d90821
Antti S. Lankila <alankila@bel.fi>
2014-06-14 11:14:38 +0300

    wav2png hack

---
+++ wav2png/README.md
@@ -0,0 +1,49 @@
+Beating FLAC in audio compression using libpng
+
+None of the audio compression codecs seem to make use of autocorrelation when
+compressing audio.  To put it shortly, autocorrelation is the property of
+signal resembling itself when shifted in time.
+
+It is formally defined as:
+
+    sum_i(f(x) * f(x - i))
+
+over some window, where i is the autocorrelation parameter.
+
+Typically, human voice singing or an instrument playing displays extremely high
+regularity across some sample window, which depends on the fundamental
+frequency of the sound. For instance, if audio is being sampled at 44100 Hz and
+contains a fundamental frequency of 440 Hz, then you will find a high
+correlation function value near i=100 because 44100 / 440 is approximately 100.
+
+Results
+-------
+
+I decided to write a simple prototype to check if I could beat FLAC in its own
+game by discovering a good autocorrelation window and using libpng to compress
+the same audio as a 16-bit grayscale image. I tested the results using the
+PAETH and UP predictors, and did some experimenting with OPTIMUM coding as
+well, but I only really got good results by using a single predictor. I settled
+with PAETH.
+
+The sample audio I chose is one of the best case signals for this kind of
+compression, a single gradually decaying piano note. It is instructive to open
+the file in image editor to see why UP or PAETH predictor will do a good job on
+it. I was able to use libpng to compress the audio to 25k using the best zlib
+compression, whereas FLAC -8 compressed it to 27k.
+
+In other words, this half an hour's work beat FLAC in this simple test by about
+10 %.
+
+What next?
+----------
+
+The current program is mere toy. It will not work for generic signals, music,
+speech, or anything like that. It will work for single signals like this,
+though. To generalize this audio encoding technique, the audio must be analyzed
+and divided into suitable chunks based on the varying autocorrelation length,
+which would then change from block to block. A stream format must be defined
+which can represent the autocorrelation parameter. More complicated prediction
+functions could be chosen than PAETH which is really designed for image
+compression.
+
---
+++ wav2png/wav2png.c
@@ -0,0 +1,93 @@
+#include <png.h>
+#include <sndfile.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <zlib.h>
+
+static uint32_t read_file(const char* filename, int16_t **data, uint32_t *length) {
+    SF_INFO info;
+    SNDFILE *sf = sf_open(filename, SFM_READ, &info);
+    if (sf == NULL) {
+        return 1;
+    }
+    *length = info.frames;
+    *data = calloc(sizeof(**data), *length);
+    sf_read_short(sf, *data, *length);
+    sf_close(sf);
+    return 0;
+}
+
+static uint32_t autocorrelate(const int16_t *data, uint32_t length) {
+    uint32_t min_window = 50;
+    uint32_t max_window = 1000;
+
+    int64_t best_correlation = 0;
+    uint32_t best_len = min_window;
+    for (uint32_t len = min_window; len <= max_window; len += 1) {
+        int64_t correlation = 0;
+        for (uint32_t i = 0; i < length - len; i += 1) {
+            correlation += (int64_t) data[i] * data[i + len];
+        }
+        fprintf(stderr, "autocorrelation: %ld for len %d %s\n", correlation, len, (correlation > best_correlation ? "*" : ""));
+        if (correlation > best_correlation) {
+            best_correlation = correlation;
+            best_len = len;
+        }
+    }
+
+    return best_len;
+}
+
+/* perform linear light alpha blending of alpha mask, write result to png */
+static void output_image(const int16_t *data, int width, int height)
+{
+    png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    png_infop info = png_create_info_struct(png);
+
+    FILE *file = fopen("test.png", "w");
+    png_init_io(png, file);
+
+    png_set_IHDR(png, info, width, height, 16, PNG_COLOR_TYPE_GRAY, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_DEFAULT);
+    png_set_compression_level(png, Z_BEST_COMPRESSION);
+    png_set_filter(png, 0, PNG_FILTER_PAETH);
+    png_bytep row = malloc(2 * width);
+
+    png_write_info(png, info);
+    for (int y = 0; y < height; y += 1) {
+        for (int x = 0; x < width; x += 1) {
+            uint16_t v = data[y * width + x] + 0x8000;
+            row[x*2+1] = v & 0xff;
+            row[x*2+0] = v >> 8;
+        }
+        png_write_row(png, row);
+    }
+    png_write_end(png, NULL);
+
+    free(row);
+    fclose(file);
+    png_destroy_write_struct(&png, &info);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <wavfile>\n", argv[0]);
+        return 1;
+    }
+
+    int16_t *data;
+    uint32_t length;
+    char *filename = argv[1];
+    if (read_file(filename, &data, &length)) {
+        fprintf(stderr, "Error reading file\n");
+        return 1;
+    }
+
+    uint32_t width = autocorrelate(data, length);
+    uint32_t height = length / width;
+
+    fprintf(stderr, "File truncated due to me being lame: %d => %d bytes, window=%d\n", length, width * height, width);
+    output_image(data, width, height);
+
+    return 0;
+}
